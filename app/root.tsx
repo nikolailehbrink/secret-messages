2539efbf6bc95593646f0fa721e6acf3af72e085
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-04-28 20:54:11 +0200

    style: update layout styles

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+      <body
+        className="flex min-h-[100dvh] flex-col text-balance bg-gradient-to-br
+          from-rose-500/10 via-sky-500/5 to-fuchsia-500/10
+          dark:from-[#6366F1]/10 dark:to-[#EC4899]/10"
+      >
+        <section className="flex w-full flex-1 py-16 md:py-32 lg:py-40">
+          {children}
+        </section>
         <ScrollRestoration />
         <Scripts />
       </body>
