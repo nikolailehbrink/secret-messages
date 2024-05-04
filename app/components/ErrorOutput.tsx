ec1f3529904e3dae24f7506d49927c3ef37b1717
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-05-04 08:44:32 +0200

    refactor: rename ErrorOutput function for better autosuggestion

--- app/components/ErrorOutput.tsx
+++ app/components/ErrorOutput.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { Warning } from "@phosphor-icons/react/dist/ssr/Warning";
 
 type Props = { message: string; className?: string };
-export default function Error({ message, className }: Props) {
+export default function ErrorOutput({ message, className }: Props) {
   return (
     <div
       className={cn("flex gap-1 rounded-md text-xs text-rose-500", className)}
