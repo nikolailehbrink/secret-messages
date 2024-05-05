da10e03e2385fb3cf14474356f6b7b5bcd4455ca
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-05-05 13:03:15 +0200

    feat(tailwind): update container padding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,11 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+      },
       screens: {
         "2xl": "1400px",
       },
