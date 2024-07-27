3304ad7f0da254f6c02d1c14d3f5f8d1175be02e
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-07-28 01:09:36 +0200

    fix: disable sentry in development

--- instrumentation.server.mjs
+++ instrumentation.server.mjs
@@ -4,4 +4,5 @@ Sentry.init({
   dsn: "https://d740eb5f0961a4f56c87e6dd81f98c6e@o4506721770733568.ingest.us.sentry.io/4507607904616448",
   tracesSampleRate: 1,
   autoInstrumentRemix: true,
+  enabled: !import.meta.env.DEV,
 });
