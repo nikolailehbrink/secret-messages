ab3264dcf448c523c84643980795e19339a71ea8
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-07-28 14:56:52 +0200

    feat: update Vite config to conditionally enable plugins
    This commit ensures that the local build of the project does not change the build output path, as discussed in https://github.com/vercel/remix/issues/125.

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,26 @@ import { vercelPreset } from "@vercel/remix/vite";
 // This installs globals such as "fetch", "Response", "Request" and "Headers". See: https://remix.run/docs/en/main/other-api/node
 installGlobals();
 
-export default defineConfig({
-  plugins: [
-    remix({
-      presets: [vercelPreset()],
-    }),
-    tsconfigPaths(),
-    sentryVitePlugin({
-      org: "nikolailehbrink",
-      project: "secretmessages",
-    }),
-  ],
+export default defineConfig(() => {
+  if (!process.env.VERCEL) {
+    return {
+      plugins: [remix(), tsconfigPaths()],
+    };
+  }
 
-  build: {
-    sourcemap: true,
-  },
+  return {
+    plugins: [
+      remix({
+        presets: [vercelPreset()],
+      }),
+      tsconfigPaths(),
+      sentryVitePlugin({
+        org: "nikolailehbrink",
+        project: "secret-messages",
+      }),
+    ],
+    build: {
+      sourcemap: true,
+    },
+  };
 });
