bf503efe1ec16f4cfc6b02b345ede5dbbce2de0d
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-06-27 15:21:11 +0200

    style: enhance button design

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,25 +5,29 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  `inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm
-  font-medium whitespace-nowrap transition-all outline-none
-  focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50
-  disabled:pointer-events-none disabled:opacity-50
+  `relative inline-flex shrink-0 cursor-pointer items-center justify-center
+  gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all
+  outline-none focus-visible:border-ring focus-visible:ring-[3px]
+  focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50
   aria-invalid:border-destructive aria-invalid:ring-destructive/20
   dark:aria-invalid:ring-destructive/40`,
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        default: `border border-foreground bg-primary text-primary-foreground
+        shadow-xs inset-shadow-2xs inset-shadow-neutral-600 before:absolute
+        before:inset-0 before:bg-linear-to-b before:from-white/20
+        before:to-transparent hover:bg-primary/90`,
         destructive: `bg-destructive text-white shadow-xs
         hover:bg-destructive/90 focus-visible:ring-destructive/20
         dark:bg-destructive/60 dark:focus-visible:ring-destructive/40`,
         outline: `border bg-background shadow-xs hover:bg-accent
         hover:text-accent-foreground dark:border-input dark:bg-input/30
         dark:hover:bg-input/50`,
-        secondary: `bg-secondary text-secondary-foreground shadow-xs
-        hover:bg-secondary/80`,
+        secondary: `border border-neutral-300 bg-secondary
+        text-secondary-foreground shadow-xs inset-shadow-2xs inset-shadow-white
+        before:absolute before:inset-0 before:bg-linear-to-b
+        before:from-background/20 before:to-transparent hover:bg-secondary/80`,
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
